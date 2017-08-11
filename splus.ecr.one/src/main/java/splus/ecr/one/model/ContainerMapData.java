1f77f136e962032d6526c3df5c5b637392ee3813
Heraj.Mishra <Heraj.Mishra@SPT-CPU-0289.splus.local>
2017-08-11 13:55:14 +0530

    ContainerMapData bean pushed

---
+++ splus.ecr.one/src/main/java/splus/ecr/one/model/ContainerMapData.java
@@ -0,0 +1,45 @@
+package splus.ecr.one.model;
+
+import java.io.Serializable;
+
+public class ContainerMapData implements Serializable{
+	
+	String title;
+	String id;
+	String color;
+	String customData;
+	String groupId;
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public String getCustomData() {
+		return customData;
+	}
+	public void setCustomData(String customData) {
+		this.customData = customData;
+	}
+	public String getGroupId() {
+		return groupId;
+	}
+	public void setGroupId(String groupId) {
+		this.groupId = groupId;
+	}
+	
+	
+
+}
